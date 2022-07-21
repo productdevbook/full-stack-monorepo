3e855b96ed49361fc1e822567df3206ce0ac67fb
productdevbook <hi@productdevbook.com>
2022-07-21 21:26:33 +0300

    Update shims-vue.d.ts

--- apps/vue-web/src/shims-vue.d.ts
+++ apps/vue-web/src/shims-vue.d.ts
@@ -2,6 +2,14 @@
 /// <reference types="@oku/api/types/schema" />
 /// <reference types="vue-app/shims-vue" />
 
+declare module '*.vue' {
+  import {DefineComponent} from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+
 interface ImportMetaEnv {
   readonly VITE_GRAPHQL_ENDPOINT: string
   readonly VITE_GRAPHQL_ENDPOINT_WS: string
